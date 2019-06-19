26cc4b962421ff63ed6426bacb40c6633d7e2843
BillGatesIsAThief <danielman1803@gmail.com>
2019-06-19 11:47:48 +0300

    primary header

---
+++ 8051/Init_Device.c
@@ -0,0 +1,76 @@
+/////////////////////////////////////
+//  Generated Initialization File  //
+/////////////////////////////////////
+
+#include "compiler_defs.h"
+#include "C8051F020_defs.h"
+
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+void Reset_Sources_Init()
+{
+    WDTCN     = 0xDE;
+    WDTCN     = 0xAD;
+}
+
+void Port_IO_Init()
+{
+    // P0.0  -  TX0 (UART0), Push-Pull,  Digital
+    // P0.1  -  RX0 (UART0), Open-Drain, Digital
+    // P0.2  -  Unassigned,  Open-Drain, Digital
+    // P0.3  -  Unassigned,  Open-Drain, Digital
+    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.5  -  Unassigned,  Open-Drain, Digital
+    // P0.6  -  Unassigned,  Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
+
+    // P1.0  -  Unassigned,  Push-Pull,  Digital
+    // P1.1  -  Unassigned,  Push-Pull,  Digital
+    // P1.2  -  Unassigned,  Push-Pull,  Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Push-Pull,  Digital
+    // P3.5  -  Unassigned,  Push-Pull,  Digital
+    // P3.6  -  Unassigned,  Push-Pull,  Digital
+    // P3.7  -  Unassigned,  Push-Pull,  Digital
+
+    P0MDOUT   = 0x01;
+    P1MDOUT   = 0x07;
+    P3MDOUT   = 0xF0;
+    XBR0      = 0x04;
+    XBR2      = 0x40;
+}
+
+void Oscillator_Init()
+{
+    int i = 0;
+    OSCXCN    = 0x66;
+    for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
+    while ((OSCXCN & 0x80) == 0);
+}
+
+// Initialization function for device,
+// Call Init_Device() from your main program
+void Init_Device(void)
+{
+    Reset_Sources_Init();
+    Port_IO_Init();
+    Oscillator_Init();
+}
